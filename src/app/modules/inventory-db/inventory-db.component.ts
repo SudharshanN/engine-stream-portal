e8d5f1f05f53f4ed0ae5e6cf561ea2e7607b6fe6
SudharshanN <sudha4954@gmail.com>
2020-10-13 18:02:22 +0530

    esp(feat) : nested table in inventory parts

--- src/app/modules/inventory-db/inventory-db.component.ts
+++ src/app/modules/inventory-db/inventory-db.component.ts
@@ -12,14 +12,24 @@ export interface PeriodicElement {
   engineType: string;
   totalQty: number
 }
-
+export interface PeriodicElement1 {
+  
+  materialNumber: string;
+  materialDescription: string;
+  engineType: string;
+}
 const ELEMENT_DATA: PeriodicElement[] = [
   {sno: 1, materialNumber: '123456A', materialDescription: '# HPC FRONT SHAFT', engineType: 'E-CFM56-7', totalQty:5},
   {sno: 2, materialNumber: '123456B', materialDescription: '# STG 1-2 SPOOL', engineType: 'E-CFM56-7', totalQty:3},
   {sno: 3, materialNumber: '123456C', materialDescription: '# HPT FRONT SHAFT', engineType: 'E-CFM56-7', totalQty:10},
   {sno: 4, materialNumber: '123456D', materialDescription: '# STG 1-2 SPOOL', engineType: 'E-CFM56-7', totalQty:10}  
 ];
-
+const ELEMENT_DATA1: PeriodicElement1[] = [
+  { materialNumber: '123456A', materialDescription: '# HPC FRONT SHAFT', engineType: 'E-CFM56-7'},
+  { materialNumber: '123456B', materialDescription: '# STG 1-2 SPOOL', engineType: 'E-CFM56-7'},
+  { materialNumber: '123456C', materialDescription: '# HPT FRONT SHAFT', engineType: 'E-CFM56-7'},
+  { materialNumber: '123456D', materialDescription: '# STG 1-2 SPOOL', engineType: 'E-CFM56-7'}  
+];
 @Component({
   selector: 'iams-inventory-db',
   templateUrl: './inventory-db.component.html',
@@ -33,12 +43,15 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class InventoryDbComponent implements OnInit,AfterViewInit  {
   displayedColumns = ['S.No', 'materialNumber', 'materialDescription', 'engineType', 'totalQty', 'Actions'];
-  dataSource: MatTableDataSource<PeriodicElement>;;
+  dataSource: MatTableDataSource<PeriodicElement>;
+  displayedColumns1 = ['materialNumber', 'materialDescription', 'engineType'];
+  dataSource1: MatTableDataSource<PeriodicElement1>;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   expandedElement: PeriodicElement | null;
   constructor() {
     this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+    this.dataSource1 = new MatTableDataSource(ELEMENT_DATA1);
    }
 
   ngOnInit(): void {
