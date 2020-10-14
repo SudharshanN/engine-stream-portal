4358981ec6d2b00ba6f2fbd8bc7d03ada601f58b
SudharshanN <sudha4954@gmail.com>
2020-10-14 14:06:56 +0530

    esp(feat) : inv parts table

--- src/app/modules/inventory-db/inventory-db.component.ts
+++ src/app/modules/inventory-db/inventory-db.component.ts
@@ -21,21 +21,36 @@ export class InventoryDbComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChildren('innerSort') innerSort: QueryList<MatSort>;
   @ViewChildren('innerTables') innerTables: QueryList<MatTable<BatchData>>;
-  sign: string = 'add';
   dataSource: MatTableDataSource<Material>;
   materialData: Material[] = [];
-  columnsToDisplay = ['select', 'sno','materialNumber', 'materialDescription', 'engineType', 'actions'];
+
+  columnsToDisplay = ['select', 'sno', 'materialNumber', 'materialDescription', 'engineType', 'plant', 'storageLocation', 'quantity', 'quantityOnHold', 'tsn', 'csn', 'condition', 'actions'];
   columnsToDisplayHeaders = {
-    materialNumber: 'Material Number', 
+    materialNumber: 'Material Number',
     materialDescription: 'Material Description',
     engineType: 'Engine Type',
+    plant: 'Plant',
+    quantity: 'Quantity',
+    quantityOnHold: 'Quantity On Hold',
+    storageLocation: 'Storage Location',
+    tsn: 'TSN',
+    csn: 'CSN',
+    condition: 'Condition',
     actions: 'Actions'
   };
-  innerDisplayedColumns = ['select', 'materialSerialNumber', 'batchNumber', 'qiBatchNumber', 'actions'];
+  innerDisplayedColumns = [
+    'select', 'materialSerialNumber', 'batchNumber', 'qiBatchNumber', 'plant', 'storageLocation', 'ageByDay', 'quantity', 'quantityOnHold', 'surplusFlag', 'actions'
+  ];
   outerColumnsHeaders = {
     materialSerialNumber: 'Material Serial Number',
     batchNumber: 'Batch Number',
     qiBatchNumber: 'Qi Batch Number',
+    plant: 'Plant',
+    quantity: 'Quantity',
+    quantityOnHold: 'Quantity On Hold',
+    storageLocation: 'Storage Location',
+    ageByDay: 'Age By Day',
+    surplusFlag: 'Surplus Flag',
     actions: 'Actions'
   }
   expandedElement: Material | null;
@@ -62,28 +77,24 @@ export class InventoryDbComponent implements OnInit, AfterViewInit {
   toggleRow(element: Material, event: Event) {
     element.batches && (element.batches as MatTableDataSource<BatchData>).data.length ? (this.expandedElement = this.expandedElement === element ? null : element) : null;
     this.cd.detectChanges();
-    this.innerTables.forEach((table, index) => (table.dataSource as MatTableDataSource<BatchData>).
-    sort = this.innerSort.toArray()[index]);
-    if (this.sign === 'add') {
-        this.sign = 'remove';
-    } else {
-        this.sign = 'add';
-    }
+    this.innerTables.
+        forEach((table, index) => (table.dataSource as MatTableDataSource<BatchData>).
+          sort = this.innerSort.toArray()[index]);
     console.log(event)
   }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-  getSalesHistory(event: Event){
+  getSalesHistory(event: Event) {
     event.stopPropagation();
   }
-  getPartFullDetails(event: Event){
+  getPartFullDetails(event: Event) {
     event.stopPropagation();
   }
-  getPartDetails(element: Material){
-    const dialogRef = this.dialog.open(BatchPartDialogComponent,  {
-      data: {...element}
+  getPartDetails(element: Material) {
+    const dialogRef = this.dialog.open(BatchPartDialogComponent, {
+      data: { ...element }
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -96,7 +107,14 @@ export class InventoryDbComponent implements OnInit, AfterViewInit {
 export interface Material {
   materialNumber: string;
   materialDescription: string;
-  engineType: string;
+  engineType?: string;
+  plant?: string;
+  storageLocation?: string;
+  quantity?: number;
+  quantityOnHold?: number;
+  tsn?: string,
+  csn?: string;
+  condition?: string;
   batches?: BatchData[] | MatTableDataSource<BatchData>;
 }
 
@@ -104,51 +122,89 @@ export interface BatchData {
   materialSerialNumber: string;
   batchNumber: string;
   qiBatchNumber: string;
+  plant?: string;
+  storageLocation?: string;
+  ageByDay?: string;
+  quantity?: number;
+  quantityOnHold?: number;
+  surplusflag?: boolean;
 }
 
 export interface UserDataSource {
   materialNumber: string;
   materialDescription: string;
   engineType: string;
+  plant?: string;
+  storageLocation?: string;
+  quantity?: number;
+  quantityOnHold?: number;
+  tsn?: string,
+  csn?: string;
+  condition?: string;
   batches?: MatTableDataSource<BatchData>;
 }
 const DATA: Material[] = [
   {
-    materialNumber: "Mason",
-    materialDescription: "mason@test.com",
-    engineType: "9864785214",
+    materialNumber: "123456A",
+    materialDescription: "# HPC FRONT SHAFT",
+    engineType: "E-CFM56-7",
+    plant: 'Test',
+    storageLocation: 'India',
+    quantity: 5,
+    quantityOnHold: 2,
+    tsn: '32DUS2233',
+    csn: '112345222',
+    condition: 'New',
     batches: [
       {
-        materialSerialNumber: "Street 1",
+        materialSerialNumber: "WE234RDS",
         batchNumber: "78542",
-        qiBatchNumber: "Kansas"
+        qiBatchNumber: "#RE10",
+        plant: 'Test4',
+        storageLocation: 'India'
       },
       {
-        materialSerialNumber: "Street 2",
+        materialSerialNumber: "432WQA",
         batchNumber: "78554",
-        qiBatchNumber: "Texas"
+        qiBatchNumber: "# WE123",
+        plant: 'test2',
+        storageLocation: 'India'
       }
     ]
   },
   {
-    materialNumber: "Eugene",
-    materialDescription: "eugene@test.com",
-    engineType: "8786541234",
+    materialNumber: "123456B",
+    materialDescription: "# STG 1-2 SPOOL",
+    engineType: "E-CFM56-7",
+    plant: 'Test1',
+    storageLocation: 'US',
+    quantity: 15,
+    quantityOnHold: 2,
+    tsn: '32DUS4433',
+    csn: '11XSD45222',
+    condition: 'New',
   },
   {
-    materialNumber: "Jason",
-    materialDescription: "jason@test.com",
-    engineType: "7856452187",
+    materialNumber: "123456C",
+    materialDescription: "# HPT FRONT SHAFT",
+    engineType: "E-CFM56-7",
+    plant: 'Test3',
+    storageLocation: 'EU',
+    quantity: 10,
+    quantityOnHold: 5,
+    tsn: '32DUS4433',
+    csn: '11XSD45222',
+    condition: 'New',
     batches: [
       {
-        materialSerialNumber: "Street 5",
+        materialSerialNumber: "12WEx",
         batchNumber: "23547",
-        qiBatchNumber: "Utah"
+        qiBatchNumber: "#023"
       },
       {
-        materialSerialNumber: "Street 5",
-        batchNumber: "23547",
-        qiBatchNumber: "Ohio"
+        materialSerialNumber: "AS001",
+        batchNumber: "23451",
+        qiBatchNumber: "#102EW"
       }
     ]
   }
