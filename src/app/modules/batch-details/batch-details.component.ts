48843426aaa52c33a30a77fb6371e44531cc64b4
Dinesh Reddy <dineshreddy.p@infosys.com>
2020-10-16 13:03:43 +0530

    batch details UI

--- src/app/modules/batch-details/batch-details.component.ts
+++ src/app/modules/batch-details/batch-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'iams-batch-details',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./batch-details.component.scss']
 })
 export class BatchDetailsComponent implements OnInit {
-
-  constructor() { }
+  material: any;
+  batchDetails: any;
+  constructor(private httpClient: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+   const materialSerialNumber = this.route.snapshot.paramMap.get('id');
+   this.httpClient.get('assets/stub/material_stub.json').subscribe((data: []) => {
+     console.log(data);
+     this.material = data;
+     this.material.forEach(m => {
+      m.batches.forEach(element => {
+        if (element.materialSerialNumber === materialSerialNumber){
+          this.batchDetails = element;
+        }
+      });
+    });
+
+    });
   }
 
 }
